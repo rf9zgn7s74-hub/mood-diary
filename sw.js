96e47310051f375c9f47faea178db98e61ae51d7
rf9zgn7s74-hub <rf9zgn7s74@privaterelay.appleid.com>
2026-09-19 13:56:26 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='mood-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./background.jpeg','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
