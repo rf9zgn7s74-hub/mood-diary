7fd3a7c9bb63630a130d7fab1e908edc26cbbd39
rf9zgn7s74-hub <rf9zgn7s74@privaterelay.appleid.com>
2026-09-19 14:20:19 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='aiwei-diary-v5';const ASSETS=['./','./index.html','./background.jpeg','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
