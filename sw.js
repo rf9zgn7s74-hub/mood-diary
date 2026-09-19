8bd11c884fe24b1cfeeec2ec31eaa691a205443f
rf9zgn7s74-hub <rf9zgn7s74@privaterelay.appleid.com>
2026-09-19 13:57:15 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='mood-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./background.jpeg','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
